cbdfcb4784942a3a79ee0d79803cd8b84bf0005c
rahul7156 <rahul7156pawar@gmail.com>
2024-05-25 16:17:25 +0530

    variable name changed

--- Practice-2.php
+++ Practice-2.php
@@ -5,14 +5,14 @@ $input_string_arr = str_split($input_string);
 
 $output_str = "";
 $current_char = "";
-$initial_char = 1;
+$initial_cnt = 1;
 
 foreach ($input_string_arr as $char) {
     if ($current_char == $char) {
-        $initial_char += 1;
+        $initial_cnt += 1;
     } else if ($current_char) {
-        $output_str .= $initial_char . "" . $current_char;
-        $initial_char = 1;
+        $output_str .= $initial_cnt . "" . $current_char;
+        $initial_cnt = 1;
     }
     $current_char = $char;
 }
